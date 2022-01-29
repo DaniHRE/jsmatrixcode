697fa38c7a80e136111942e4275746e91f8327b2
Daniel Henrique <62768034+DaniHRE@users.noreply.github.com>
2022-01-28 23:40:29 -0300

    Functions added to program.
    Press letter "M" to chance modules in the page.
    
    Alter with normal, rainbow and fun mode.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 let modeIndex = 0;
 
+let playerx = 0;
+let playery = 0;
+
 function normal (){
     return 'lightgreen';
 }
@@ -15,9 +18,21 @@ function rainbow(){
     return modeStyle;
 }
 
+function circle(){
+    const modeStyle = context.createRadialGradient(canvas.width/2 + playery, canvas.height/2 + playerx, 0, canvas.width/2 + playery, canvas.height/2 + playerx, 200)
+
+    for (let i = 0; i < randomColors.length; i++){
+        let offset = i/randomColors.length
+        modeStyle.addColorStop(offset, randomColors[i]);
+    } 
+
+    return modeStyle;
+}
+
 const MODES = [
     normal,
-    rainbow
+    rainbow,
+    circle
 ]
 
 function changeMode(){
@@ -32,6 +47,25 @@ window.addEventListener('keydown', event =>{
     if(event.key == 'm') {
         changeMode()
     }
+
+    if(modeIndex == 2){
+        if(event.key == 'w'){
+            playerx = playerx - 5
+        }
+    
+        if(event.key == 's'){
+            playerx = playerx + 5
+        }
+    
+        if(event.key == 'd'){
+            playery = playery + 5
+        }
+    
+        if(event.key == 'a'){
+            playery = playery - 5
+        }
+    }
+    
 })
 
 const canvas = document.getElementById('Matrix');
@@ -40,7 +74,7 @@ const context = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-const random = '👺👹⛩🍙';
+const random = '👺';
 const katakana = 'クケコサシスセソタヂッツデトドナニヌネバヒピプヘ';
 const latin = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const num = '0123456789';
@@ -83,11 +117,11 @@ const draw = () => {
         const text = alphabet.charAt(Math.floor(Math.random()*alphabet.length));
         context.fillText(text, i * fontSize, rainDrops[i] * fontSize);
 
-        if (rainDrops[i] * fontSize > canvas.height && Math.random() > 0.975) {
+        if (rainDrops[i] * fontSize > canvas.height && Math.random() > 0.9) {
             rainDrops[i] = 0;
         }
         rainDrops[i]++;
     }
 };
 
-setInterval(draw, 30);
+setInterval(draw, 10);
